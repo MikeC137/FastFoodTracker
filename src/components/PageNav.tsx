0c5bb5b42cf26789ed6b9c41b1f2d79ab2f681b6
MikeC137 <michalischatzinikolas13@gmail.com>
2025-11-20 17:02:09 -0500

    feat: enhance PageNav with navigation links for Dashboard and History

--- src/components/PageNav.tsx
+++ src/components/PageNav.tsx
@@ -1,10 +1,27 @@
+import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
 
 function PageNav() {
   return (
-    <nav className="bg-zinc-800 px-4 py-3">
+    <nav className="bg-zinc-800 px-4">
       <div className="mx-auto flex max-w-5xl items-center justify-between">
         <Logo />
+        <ul className="md:flex flex items-center gap-4 text-zinc-300 font-inter">
+          <li
+            className={
+              "hover:text-white hover:bg-blue-500 p-2 rounded-lg transition-colors"
+            }
+          >
+            <NavLink to="/dashboard">DASHBOARD</NavLink>
+          </li>
+          <li
+            className={
+              "hover:text-white hover:bg-blue-500 p-2 rounded-lg transition-colors"
+            }
+          >
+            <NavLink to="/history">HISTORY</NavLink>
+          </li>
+        </ul>
       </div>
     </nav>
   );
